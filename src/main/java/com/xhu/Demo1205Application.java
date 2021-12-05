cf9bc5aca59d04a0f3ad1707c025b2a1c6779dd7
yuxuanmian <1214386527@qq.com>
2021-12-05 11:00:44 +0800

    Initial commit

---
+++ src/main/java/com/xhu/Demo1205Application.java
@@ -0,0 +1,13 @@
+package com.xhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1205Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhu/Demo1205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1205ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
